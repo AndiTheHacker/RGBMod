a85665fc632cd432084c70f0428db1f375d4b47e
andi <andiderhacker@outlook.de>
2023-01-21 14:45:52 +0100

    works now. hopefully. sucks though because threads

--- RGBMod/src/main/kotlin/com/andi/rgb/RGBMod.kt
+++ RGBMod/src/main/kotlin/com/andi/rgb/RGBMod.kt
@@ -2,43 +2,39 @@ package com.andi.rgb
 
 import io.gitlab.mguimard.openrgb.client.OpenRGBClient
 import io.gitlab.mguimard.openrgb.entity.OpenRGBColor
+import io.gitlab.mguimard.openrgb.entity.OpenRGBDevice
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents
-import net.fabricmc.fabric.api.event.Event
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
-import net.minecraft.util.ActionResult
 
 @Suppress("UNUSED")
 object RGBMod: ModInitializer {
     private const val MOD_ID = "rgb"
+    private val client: OpenRGBClient = OpenRGBClient("localhost", 6742, "minecraft")
+    private var cIndex: Int = 0
+    private var controller: OpenRGBDevice = OpenRGBDevice()
+
     override fun onInitialize() {
         println("RGBMod has been initialized.")
 
-        val client = OpenRGBClient("localhost", 6742, "minecraft")
         try {
             client.connect()
+            cIndex =  client.controllerCount - 2
+            controller = client.getDeviceController(cIndex)
         } catch (e: java.lang.Exception) {
             e.printStackTrace()
         }
 
-        val cIndex = client.controllerCount - 1
-        val controller = client.getDeviceController(cIndex)
-
         client.updateMode(cIndex, 17, controller.modes[17])
-
-        PlayerBlockBreakEvents.AFTER.register(PlayerBlockBreakEvents.After { world, player, pos, state, blockEntity ->
-
+    }
+    fun damage() {
+        Thread {
             client.updateMode(cIndex, 1, controller.modes[1])
-            client.updateLeds(cIndex, Array(controller.colors.size) {OpenRGBColor.fromHexaString("#00FFFF")})
+            client.updateLeds(cIndex, Array(controller.colors.size) { OpenRGBColor.fromHexaString("#00FFFF") })
             Thread.sleep(300)
-            client.updateMode(client.controllerCount-1, 17, client.getDeviceController(client.controllerCount-1).modes[17])
-        })
-        PlayerCallback.DAMAGE.register(PlayerCallback { player ->
-            client.updateMode(cIndex, 1, controller.modes[1])
-            client.updateLeds(cIndex, Array(controller.colors.size) {OpenRGBColor.fromHexaString("#00FFFF")})
-            Thread.sleep(300)
-            client.updateMode(client.controllerCount-1, 17, client.getDeviceController(client.controllerCount-1).modes[17])
-            return@PlayerCallback ActionResult.SUCCESS
-        })
+            client.updateMode(
+                cIndex,
+                17,
+                controller.modes[17]
+            )
+        }.start()
     }
 }
--- RGBMod/src/main/kotlin/com/andi/rgb/mixins/PlayerMixin.kt
+++ RGBMod/src/main/kotlin/com/andi/rgb/mixins/PlayerMixin.kt
@@ -1,23 +1,22 @@
 package com.andi.rgb.mixins
 
-import com.andi.rgb.PlayerCallback
+import com.andi.rgb.RGBMod
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
 @Mixin(PlayerEntity::class)
 abstract class PlayerMixin {
     @Inject(
         at = [At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/damage/DamageTracker;onDamage(Lnet/minecraft/entity/damage/DamageSource;FF)V",
-            shift = At.Shift.AFTER
+            value = "RETURN"
         )], method = ["applyDamage"]
     )
-    private fun onDamage(source: DamageSource, amount: Float, ci: CallbackInfo) {
-        val result = PlayerCallback.DAMAGE.invoker().damage(this as PlayerEntity)
+    private fun dmg(source: DamageSource, amount: Float, ci: CallbackInfo) {
+    RGBMod.damage()
     }
 
 
